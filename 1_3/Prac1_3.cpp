23b0be96ce84c4e039ce3b745cdc0111a7d61d22
JHG@LT <rpg2657@gmail.com>
2026-04-11 17:37:48 +0900

    Prac 1

---
+++ 1_3/Prac1_3.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(vector<int> a, vector<vector<int>> edges) {
+    long long answer = -2;
+    return answer;
+}
---
+++ 1_3/Prac1_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int> > solution(int n, vector<vector<int> > signs)
+{
+    vector<vector<int> > answer;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
